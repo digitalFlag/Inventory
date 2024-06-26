e08fbfa3bc413580494a9d8f808b27bb86a05f3b
digitalFlag <gelpostbox@gmail.com>
2024-06-26 07:01:33 +0300

    Product and Order Models Has Been Created.

---
+++ Models/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace Inventory.Models
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Inventory.Models
+{
+    class Product : Order
+    {
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public DateOnly ExpirationDate { get; set; }
+
+        public string Location { get; set; }
+    }
+}
