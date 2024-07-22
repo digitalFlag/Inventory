761a003e6c51c3814ec5b4cde1f9e520d01921c4
digitalFlag <gelpostbox@gmail.com>
2024-07-22 06:36:53 +0300

    New ViewModel "Add New Product To Warehouse" Created.

---
+++ ViewModels/Warehouse/AddNewProductToWarehouseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Inventory.ViewModels.Base;
+
+namespace Inventory.ViewModels.Warehouse
+{
+    public partial class WarehouseViewModel : DialogViewModel
+    {
+        #region Dependencies
+
+        #endregion
+
+
+        #region Commands
+        #endregion
+    }
+}
