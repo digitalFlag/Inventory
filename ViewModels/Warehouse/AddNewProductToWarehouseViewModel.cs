74dfc030a183bf98c778a633dc7a5539cdb4421e
digitalFlag <gelpostbox@gmail.com>
2024-07-30 06:59:12 +0300

    "Add new Warehouse Product" Method Is Creating.

--- ViewModels/Warehouse/AddNewProductToWarehouseViewModel.cs
+++ ViewModels/Warehouse/AddNewProductToWarehouseViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Inventory.Commands;
+using Inventory.Models;
+using Inventory.Resources.Constants;
 using Inventory.ViewModels.Base;
 using System.Windows.Input;
 
@@ -231,8 +233,215 @@ namespace Inventory.ViewModels.Warehouse
 
 		private void OnPushButtonAddToWarehouseAtAddToWarehousePanelCommandExecuted(object? p)
 		{
-			//ToDo Need to Emplement
-		}
+            var dbSettings = new DBSettings
+            {
+                Server = ConnectionOptions.dbServer,
+                Port = ConnectionOptions.dbPort,
+                Name = ConnectionOptions.dbName,
+                UserId = ConnectionOptions.userId,
+                Password = ConnectionOptions.password
+            };
+
+            string tableTittle = Resources.Constants.WarehouseProducts.tableTittleWarehouseProducts;
+
+            WarehouseProduct warehouseProduct = new();
+            warehouseProduct.Tittle = TittleProductAddNewToWarehouse;
+            warehouseProduct.Property = PropertyProductAddNewToWarehouse;
+            warehouseProduct.Property = SizeProductAddNewToWarehouse;
+            warehouseProduct.ExpirationDate = new DateTime(2008, 6, 1, 7, 47, 0);
+            warehouseProduct.PurchaseCost = Convert.ToInt16(PurchaseCostAddNewToWarehouse);
+            warehouseProduct.Location = LocationAddNewToWarehouse;
+            warehouseProduct.ReceiptDate = new DateTime(2028, 6, 1, 7, 47, 0); ;
+            warehouseProduct.OrderNumber = OrderNumberAddNewToWarehouse;
+            warehouseProduct.Note = NoteAddNewToWarehouse;
+
+            //ToDo I am Here.
+
+
+
+
+
+
+
+
+
+
+
+            if (_DataBase is null)
+            {
+                return;
+            }
+
+            string id = SelectedWarehouseProduct.Id.ToString();
+
+            if (id is null)
+            {
+                return;
+            }
+
+            //Tittle
+            if (BorderColorSelectedProductTittleMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyTittle;
+
+                if (SelectedProductTittle is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductTittle);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение названия выбранного продукта изменено.";
+                BorderColorSelectedProductTittleMyWarehouseControlTab = "HotPink";
+            }
+            //Property
+            if (BorderColorSelectedProductPropertyMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyProperty;
+
+                if (SelectedProductProperty is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductProperty);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductPropertyMyWarehouseControlTab = "HotPink";
+            }
+            //Size
+            if (BorderColorSelectedProductSizeMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertySize;
+
+                if (SelectedProductSize is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductSize);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductSizeMyWarehouseControlTab = "HotPink";
+
+
+
+
+
+            }
+            //Expiration Data
+            if (BorderColorSelectedProductExpirationDateMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyExpirationDate;
+
+                if (SelectedProductExpirationData is null)
+                {
+                    return;
+                }
+
+                string newValue = "01." + SelectedProductExpirationData.Substring(0, 2) + '.' + SelectedProductExpirationData.Substring(3, 4);
+
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, newValue);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductExpirationDateMyWarehouseControlTab = "HotPink";
+
+
+
+
+
+            }
+            //Purchase Cost
+            if (BorderColorSelectedProductPurchaseCostMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyPurchaseCost;
+
+                if (SelectedProductPurchaseCost is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductPurchaseCost);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductPurchaseCostMyWarehouseControlTab = "HotPink";
+
+
+
+
+
+            }
+            //Location
+            if (BorderColorSelectedProductLocationMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyLocation;
+
+                if (SelectedProductLocation is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductLocation);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductLocationMyWarehouseControlTab = "HotPink";
+            }
+            //Receipt Date
+            if (BorderColorSelectedProductReceiptDateMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyReceiptDate;
+
+                if (SelectedProductReceiptDate is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductReceiptDate);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductReceiptDateMyWarehouseControlTab = "HotPink";
+
+
+
+
+
+            }
+            //Order Number
+            if (BorderColorSelectedProductOrderNumberMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyOrderNumber;
+
+                if (SelectedProductOrderNumber is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductOrderNumber);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductOrderNumberMyWarehouseControlTab = "HotPink";
+            }
+            //Note
+            if (BorderColorSelectedProductNoteMyWarehouseControlTab == "Green")
+            {
+                string columnTittle = WarehouseTable.propertyNote;
+
+                if (SelectedProductNote is null)
+                {
+                    return;
+                }
+
+                _DataBase.UpdateRecord(dbSettings, tableTittle, columnTittle, id, SelectedProductNote);
+
+                TextLabelEventLogMyWarehouseTabControlWarehouseWindow = "Значение свойства выбранного продукта изменено.";
+                BorderColorSelectedProductNoteMyWarehouseControlTab = "HotPink";
+            }
+
+
+
+
+        }
 
 		#endregion
 
