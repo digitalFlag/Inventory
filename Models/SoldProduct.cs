e350c2b3a1f52f20c42aa35744d7e4eacd0c5145
digitalFlag <gelpostbox@gmail.com>
2024-09-14 08:08:33 +0300

    Sold Product Model Created.

---
+++ Models/SoldProduct.cs
@@ -0,0 +1,17 @@
+﻿namespace Inventory.Models
+{
+    public class SoldProduct : Product
+    {
+        public int? Id { get; set; }
+
+        public int? PurchaseCost { get; set; }
+
+        public int? SoldCost { get; set; }
+
+        public DateTime? SoldDate { get; set; }
+
+        public int? CustomerId { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
--- ViewModels/Warehouse/SoldProductsViewModel.cs
+++ ViewModels/Warehouse/SoldProductsViewModel.cs
@@ -1,10 +1,39 @@
-﻿using Inventory.ViewModels.Base;
+﻿using Inventory.Models;
+using Inventory.ViewModels.Base;
+using System.Data;
 
 namespace Inventory.ViewModels.Warehouse
 {
     public partial class WarehouseViewModel : DialogViewModel
     {
         #region Dependencies
+
+        #region LoadedDataTableFromDataBaseSoldProducts: - Loaded "DataTable" From SQL DB For "Sold Products Window"
+
+        /// <summary>Loaded "DataTable" From SQL DB For "Sold Products Window"</summary>
+        private DataTable? _LoadedDataTableFromDataBaseSoldProducts;
+
+        /// <summary>Loaded "DataTable" From SQL DB For "Sold Products Window"</summary>
+
+        public DataTable? LoadedDataTableFromDataBaseSoldProducts { get => _LoadedDataTableFromDataBaseSoldProducts; set => Set(ref _LoadedDataTableFromDataBaseSoldProducts, value); }
+
+        #endregion
+
+        #region SoldProducts: - Products On Sold List
+
+        /// <summary>Products On Sold List</summary>
+        private IEnumerable<SoldProduct>? _SoldProducts;
+
+        /// <summary>Products On Sold List</summary>
+
+        public IEnumerable<SoldProduct>? SoldProducts { get => _SoldProducts; set => Set(ref _SoldProducts, value); }
+
+        #endregion
+
+
+
+
+
 
         #endregion
 
